c88d2c2cc9d0e25b7b6e5b828508195c3c3db490
Mateo Peñaranda <mateo_1999_7@outlook.es>
2023-01-13 00:36:35 -0500

    Update index.js

--- pickle-rick-front/src/router/index.js
+++ pickle-rick-front/src/router/index.js
@@ -17,14 +17,6 @@ const router = createRouter({
       props: true,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue')
-    },
-    {
         path: "/:pathxatch(.*)*",
         redirect: "/character"    
     }
